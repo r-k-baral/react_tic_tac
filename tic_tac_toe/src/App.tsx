31f4f6cecce707f6db7dbdf90d04922acd319568
r-k-baral <160538972+r-k-baral@users.noreply.github.com>
2026-04-21 20:41:48 +0530

    working tic tac to

--- tic_tac_toe/src/App.tsx
+++ tic_tac_toe/src/App.tsx
@@ -4,13 +4,44 @@ import Block from "./components/Block";
 
 function App(){
   const [state, setState] = useState(Array(9).fill(null));
-  const [palayerturn, setpalayerturn] = useState("X");
+  const [playerturn, setplayerturn] = useState("X");
+
+
+  const checkWhowin = (state : any[]) =>{
+    const win = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ];
+    for(let i = 0; i < win.length; i++){
+      const [a,b,c] = win[i];
+      if((state[a] !== null && state[a] === state[b] && state[a] === state[c])) return true ;
+    }
+    return false
+  };
   const  Handlclick  = (index: number) => {
     console.log(index);
     const statecopy = Array.from(state)
-    statecopy[index] = palayerturn
-    setpalayerturn(palayerturn === 'X' ? 'O' : 'X')
+
+    if(statecopy[index] !== null) return
+    statecopy[index] = playerturn
+    
+    // check if someone win
+
+    const win = checkWhowin(statecopy);
+
+    if(win){
+      alert(`${playerturn} wom the game`)
+    }
+
+    setplayerturn(playerturn === 'X' ? 'O' : 'X')
     setState(statecopy);
+    
         
   };
   console.log(state);
